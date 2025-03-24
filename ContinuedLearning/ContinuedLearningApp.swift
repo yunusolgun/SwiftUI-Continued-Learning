8ab8ef067affac2c368e6833930280ffe24bbfb4
Yunus Olgun <y.olgun@gmail.com>
2025-03-24 22:06:26 +0300

    Initial Commit

---
+++ ContinuedLearning/ContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuedLearningApp.swift
+//  ContinuedLearning
+//
+//  Created by yunus olgun on 24.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
